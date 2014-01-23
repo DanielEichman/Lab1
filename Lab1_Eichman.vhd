e60bc99356225169224189d7cfc82672c34ef8ae
Daniel Eichman <C16Daniel.Eichman@usafa.edu>
2014-01-22 23:19:10 -0700

    Added boolean logic not yet tested

---
+++ Lab1_Eichman.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    22:59:00 01/22/2014 
+-- Design Name: 
+-- Module Name:    Lab1_Eichman - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Lab1_Eichman is
+end Lab1_Eichman;
+
+architecture Behavioral of Lab1_Eichman is
+
+begin
+
+
+end Behavioral;
+
