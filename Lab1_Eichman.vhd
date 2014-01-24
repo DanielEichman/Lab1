a070be31754ec7b7dfab5465e919c32343ab0ef2
Daniel Eichman <C16Daniel.Eichman@usafa.edu>
2014-01-24 14:02:11 -0700

    updated

--- Lab1_Eichman.vhd
+++ Lab1_Eichman.vhd
@@ -1,11 +1,11 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Company: Science Ltd.
+-- Engineer: Daniel Eichman
 -- 
 -- Create Date:    22:59:00 01/22/2014 
--- Design Name: 
+-- Design Name: The coolest design ever!
 -- Module Name:    Lab1_Eichman - Behavioral 
--- Project Name: 
+-- Project Name: Lab01
 -- Target Devices: 
 -- Tool versions: 
 -- Description: 
@@ -30,12 +30,39 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity Lab1_Eichman is
+	Port(A:in STD_LOGIC;
+		  B:in STD_LOGIC;
+		  C:in STD_LOGIC;
+		  X:out STD_LOGIC;
+		  Y:out STD_LOGIC;
+		  Z:out STD_LOGIC);
 end Lab1_Eichman;
 
 architecture Behavioral of Lab1_Eichman is
-
+--for all
+signal A_NOT,B_NOT,C_NOT: STD_LOGIC;
+--for X
+signal D,E,F,G,H: STD_LOGIC;
+--for Y
+signal I,J: STD_LOGIC;
+--for Z
 begin
-
-
+--for all
+A_NOT <= not A;
+B_NOT <= not B;
+C_NOT <= not C;
+--for X
+D<=A_NOT and C;
+E<=D or F;
+F<=A_NOT and B;
+G<=A and B_NOT;
+H<=G and C_NOT;
+X<=E or H;
+--for Y
+I<=B_NOT and C;
+J<=B and C_NOT;
+Y<=I or J;
+--for Z
+Z<=C;
 end Behavioral;
 
